797965b0fec38c97221378823b029c2dcb308ef7
Arsh <arshheer781@gmail.com>
2025-08-27 10:48:17 +0530

    feat- add grid for dijkstra.

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -56,11 +56,11 @@ function getAllNodes(grid) {
 // Backtracks from the finishNode to find the shortest path.
 // Only works when called *after* the dijkstra method above.
 export function getNodesInShortestPathOrder(finishNode) {
-  const nodesInShortestPathOrder = [];
+  const nodesInShortestPath = [];
   let currentNode = finishNode;
   while (currentNode !== null) {
-    nodesInShortestPathOrder.unshift(currentNode);
+    nodesInShortestPath.unshift(currentNode);
     currentNode = currentNode.previousNode;
   }
-  return nodesInShortestPathOrder;
+  return nodesInShortestPath;
 }
